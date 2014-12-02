09f5bf5cd3e513b1d03f3848921fccd2328eec82
Jesse Smick <janka102@gmail.com>
2014-12-02 15:28:12 -0800

    Moved email html to separate file

--- email.js
+++ email.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird'),
     util = require('util'),
     mailOptions = {
         subject: 'WebMonitor - %s',
-        html: '<div style="width:100%;background-color:#e3e3e3;padding:35px;"><div style="color:#222;margin:auto;font-size:18px;font-family:sans-serif;max-width:650px;background-color:#f7f7f7;"><h2 style="color:#efefef;font-size:30px;background-color:#345;padding:25px 30px;margin:0;vertical-align:middle;">%s</h2><div style="display:block;padding:30px;"><span style="display:block;margin-bottom:40px">A value on your page <b><a href="%s" style="color:#456;">%s</a></b> changed.</span><span style="display:block;">It was:</span><span style="display:block;background-color:#e7dddd;border: 1px solid #dcc;padding:10px;margin:15px 25px 35px;">%s</span><span style="display:block;">It is now:</span><span style="display:block;background-color:#dde7dd;border: 1px solid #cdc;padding:10px;margin:15px 25px 35px;">%s</span></div><div style="display:block;font-size:13px;padding:15px;color:#222;background-color:#c0c8cf;text-align:center;font-weight:bold;border-top: 1px solid #abc;"><a href="%s" style="color:#456;">WebMonitor</a> &#8226; <a href="https://github.com/janka102/webmonitor" style="color:#456;">Source on GitHub</a> &#8226; <a href="%s/stop/%s" style="color:#456;">Stop monitoring this value</a></div></div></div>'
+        html: require('fs').readFileSync('./views/email.html').toString()
     },
     user = require('./user.json'),
     transporter = nodemailer.createTransport({
