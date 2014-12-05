8db436b6f2239f2384fa6d92dbb57031bced3dd2
Jesse Smick <janka102@gmail.com>
2014-12-04 17:24:27 -0800

    Use only one quote type

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
     port: parseInt(process.env.PORT || 3000),
     dev: (process.env.NODE_ENV || 'development') === 'development',
     email: {
-        service: "",
-        user: "",
-        pass: "",
-        fromEmail: "",
-        toEmail: ""
+        service: '',
+        user: '',
+        pass: '',
+        fromEmail: '',
+        toEmail: ''
     },
-    domain: ""
+    domain: ''
 };
