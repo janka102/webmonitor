0db7af6c773db8539c459905a2d0d6a3a18a0fb7
Jesse Smick <janka102@gmail.com>
2017-11-03 19:25:50 -0700

    Format server file, update dependencies, use dotenv, and use nunjucks in place of swig

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
-module.exports = {
-    port: parseInt(process.env.PORT || 3000),
-    dev: (process.env.NODE_ENV || 'development') === 'development',
-    email: {
-        service: '',
-        user: '',
-        pass: '',
-        fromEmail: '',
-        toEmail: '',
-        toConsole: false
-    },
-    domain: ''
-};
+exports = module.exports = {
+  root: __dirname,
+  production: process.env.NODE_ENV === 'production',
+  port: process.env.PORT || 3000,
+  domain: process.env.MONITOR_DOMAIN,
+  email: {
+    service: process.env.EMAIL_SERVICE,
+    username: process.env.EMAIL_USER,
+    password: process.env.EMAIL_PASSWORD,
+    to: process.env.EMAIL_TO,
+    from: process.env.EMAIL_FROM,
+    consoleOnly: false
+  }
+}
+
+if (!exports.production) {
+  exports.email.consoleOnly = true
+}
+
+if (!exports.domain) {
+  exports.domain = `http://localhost:${exports.port}`
+}
--- server.js
+++ server.js
@@ -1,128 +1,152 @@
-var express = require('express'),
-    app = express(),
+// Add items to `process.env`
+require('dotenv').config()
 
-    morgan = require('morgan'),
-    compression = require('compression'),
-    bodyParser = require('body-parser'),
-    swig = require('swig'),
-
-    path = require('path'),
-    util = require('util'),
-    config = require('./config'),
-    jobs = require('./jobs');
-
-// Add the port to the domain
-if (config.port !== 80 && config.port !== 443) {
-    config.domain += ':' + config.port;
-}
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const nunjucks = require('nunjucks')
+const path = require('path')
+const util = require('util')
+const config = require('./config.js')
+const jobs = require('./jobs')
+const app = express()
 
 // Setup the server
-app.use(compression());
+if (!config.production) {
+  app.use(morgan('dev'))
+}
 
-app.use(morgan(config.dev ? 'dev' : 'tiny'));
-
-app.use(bodyParser.urlencoded({
+app.disable('x-powered-by')
+app.use(compression())
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+)
 
-swig.setDefaults({
-    cache: config.dev ? false : 'memory',
-    locals: {
-        domain: config.domain
-    }
-});
+// Set "./views" as the views folder
+nunjucks.configure('views', {
+  express: app,
+  noCache: !config.production
+})
 
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+// Set default template extension to .html
+app.set('view engine', 'html')
+
+app.locals.css = ['/main.css']
+app.locals.js = []
+app.locals.domain = config.domain
 
 // Public files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(config.root, 'public')))
 
 app.get('/', function(req, res) {
-    res.render('index', {
-        dev: config.dev
-    });
-});
+  res.render('index', {
+    production: config.production,
+    css: req.app.locals.css.concat('/index.css')
+  })
+})
 
 app.get('/list', function(req, res) {
-    jobs.find({}, {
+  jobs
+    .find(
+      {},
+      {
         _id: 0,
         id: 1,
         title: 1,
         url: 1
-    }).toArray(function(err, array) {
-        if (err) {
-            console.error('Find error:', err);
-            res.render('error', {
-                error: {
-                    title: 'Internal error',
-                    name: 'Could not create monitored value'
-                }
-            });
-            return;
-        }
+      }
+    )
+    .toArray(function(err, array) {
+      if (err) {
+        console.error('Find error:', err)
+        res.render('error', {
+          error: {
+            title: 'Internal error',
+            name: 'Could not create monitored value'
+          }
+        })
+        return
+      }
 
-        res.render('list', {
-            jobs: array
-        });
-    });
-});
+      res.render('list', {
+        jobs: array
+      })
+    })
+})
 
 app.post('/monitor', function(req, res) {
-    jobs.create(req.body).then(function(job) {
-        res.send(200);
-    }, function(err) {
-        res.send(500);
-    });
-});
+  jobs.create(req.body).then(
+    function(job) {
+      res.send(200)
+    },
+    function(err) {
+      res.send(500)
+    }
+  )
+})
 
-app.route('/stop/:id')
-    .get(function(req, res) {
-        jobs.findOne({
-            id: req.params.id
-        }).then(function(job) {
-            res.render('stop', {
-                job: job
-            });
-        }, function(err) {
-            res.render('error', {
-                error: {
-                    title: 'Not found',
-                    name: 'Could not find specified monitor value',
-                    description: 'There is no current monitor value with id "<b>' + req.params.id + '</b>"'
-                }
-            });
-        });
-    })
-    .post(function(req, res) {
-        jobs.findOne({
-            id: req.params.id
-        }).then(function(job) {
-            jobs.remove(job);
+app
+  .route('/stop/:id')
+  .get(function(req, res) {
+    jobs
+      .findOne({
+        id: req.params.id
+      })
+      .then(
+        function(job) {
+          res.render('stop', {
+            job: job
+          })
+        },
+        function(err) {
+          res.render('error', {
+            error: {
+              title: 'Not found',
+              name: 'Could not find specified monitor value',
+              description: 'There is no current monitor value with id "<b>' + req.params.id + '</b>"'
+            }
+          })
+        }
+      )
+  })
+  .post(function(req, res) {
+    jobs
+      .findOne({
+        id: req.params.id
+      })
+      .then(
+        function(job) {
+          jobs.remove(job)
 
-            res.send(200);
-        }, function(err) {
-            res.status(404).end(JSON.stringify({
-                error: err
-            }));
-        });
-    });
+          res.send(200)
+        },
+        function(err) {
+          res.status(404).end(
+            JSON.stringify({
+              error: err
+            })
+          )
+        }
+      )
+  })
 
 // Catch-all/404
-app.get('*', function(req, res) {
-    res.render('error', {
-        error: {
-            title: 'Not Found',
-            name: '404 - Page not found',
-            description: 'The page at <b>' + req.path + '</b> was not found. Go <a href="' + config.domain + '">home</a>.'
-        }
-    })
-});
+app.use((req, res, next) => {
+  res.status(404)
+  res.render('error', {
+    error: {
+      title: 'Not Found',
+      name: '404 - Page not found',
+      description: 'The page at <b>' + req.path + '</b> was not found. Go <a href="' + config.domain + '">home</a>.'
+    }
+  })
+})
 
 // Open the port for business
-app.listen(config.port, function() {
-    console.log('Now running on port %d', config.port);
-});
-
-jobs.startAll();
+app.listen(config.port, () => {
+  console.log(`=> Running on ${config.domain}`)
+  jobs.startAll()
+})
