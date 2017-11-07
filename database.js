241c57f3659de65c46c7f13be61b7981fc30b65a
Jesse Smick <janka102@gmail.com>
2017-11-07 01:08:47 -0800

    Fix mongoose promise .catch issue and add paused to jobSchema

--- database.js
+++ database.js
@@ -3,38 +3,51 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const config = require('./config.js')
 
-mongoose.Promise = Promise
+// Tungus only supports mongoose 3.x
+// mongoose 3.x uses mpromise and doesn't support mongoose.Promise = Promise
+// and mpromise doesn't have .catch, so have to do this hack to get it to work
+// see https://github.com/aheckmann/mpromise/pull/14#issuecomment-68448406
+require('mpromise').prototype.catch = function (onReject) {
+  return this.then(undefined, onReject);
+};
 
 module.exports = function(cb) {
   mongoose.connect(config.mongoose)
   const db = mongoose.connection
 
   // Register models
-  const jobSchema = Schema({
-    title: String,
-    url: {
-      type: String,
-      match: [/^https?:/, 'URL is not HTTP(S) ({VALUE})']
-    },
-    query: {
-      mode: {
+  mongoose.model(
+    'Job',
+    Schema({
+      title: String,
+      url: {
         type: String,
-        enum: ['query', 'regex']
+        match: [/^https?:/, 'URL is not HTTP(S) ({VALUE})']
       },
-      selector: String
-    },
-    interval: String,
-    values: [{
-      time: Date,
-      value: String,
-      kind: {
-        type: String,
-        enum: ['number', 'text', 'error']
-      }
-    }]
-  })
-
-  mongoose.model('Job', jobSchema)
+      query: {
+        mode: {
+          type: String,
+          enum: ['query', 'regex']
+        },
+        selector: String
+      },
+      interval: String,
+      paused: {
+        type: Boolean,
+        default: false
+      },
+      values: [
+        {
+          time: Date,
+          value: String,
+          kind: {
+            type: String,
+            enum: ['number', 'text', 'error']
+          }
+        }
+      ]
+    })
+  )
 
   db.on('error', cb)
   db.once('open', () => {
