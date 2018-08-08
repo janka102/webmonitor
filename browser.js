249b7d5001c339e9a48b2f4cd72fe4d2de3c8640
Jesse Smick <janka102@gmail.com>
2018-08-07 18:48:35 -0700

    Update puppeteer code

--- browser.js
+++ browser.js
@@ -1,86 +1,53 @@
-const puppeteer = require('puppeteer')
-
-let browser = null
-const backlog = []
+const puppeteer = require('puppeteer');
 
 exports = module.exports = {
-  execute(job) {
-    if (!browser) {
-      return new Promise((resolve, reject) => {
-        const fn = (error, result) => {
-          if (error) {
-            return reject(error)
-          }
+  async execute (job) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-          resolve(result)
+    let result = null;
+    let error = null;
+
+    try {
+      await page.goto(job.url, { timeout: 15 * 1000, waitUntil: 'networkidle' });
+
+      result = await page.evaluate(query => {
+        let search = '';
+
+        if (query.mode === 'query') {
+          const el = document.querySelector(query.selector);
+          search = el ? el.textContent.trim() : '';
+        } else if (query.mode === 'regex') {
+          const match = document.body.textContent.match(new RegExp(query.selector));
+          search = match ? match[0].trim() : '';
         }
 
-        backlog.push({ job, fn })
-      })
+        const number = Number(search);
+
+        if (search.length && !isNaN(number)) {
+          search = number;
+        }
+
+        return search;
+      }, job.query);
+    } catch (e) {
+      error = e;
     }
 
-    return execute(job)
-  }
-}
+    await browser.close();
 
-puppeteer.launch().then(newBrowser => {
-  browser = newBrowser
+    if (error) {
+      throw {
+        time: new Date(),
+        value: error.message,
+        kind: 'error'
+      };
+    }
 
-  for (const { job, fn } of backlog) {
-    exports
-      .execute(job)
-      .then(result => fn(null, result))
-      .catch(error => fn(error))
-  }
-
-  backlog.length = 0
-})
-
-async function execute(job) {
-  const page = await browser.newPage()
-  let numRequests = 0
-  let result = null
-  let error = null
-
-  try {
-    await page.goto(job.url, { timeout: 15 * 1000, waitUntil: 'networkidle' })
-
-    result = await page.evaluate(query => {
-      let search = ''
-
-      if (query.mode === 'query') {
-        const el = document.querySelector(query.selector)
-        search = el ? el.textContent.trim() : ''
-      } else if (query.mode === 'regex') {
-        const match = document.body.textContent.match(new RegExp(query.selector))
-        search = match ? match[0].trim() : ''
-      }
-
-      const number = Number(search)
-
-      if (search.length && !isNaN(number)) {
-        search = number
-      }
-
-      return search
-    }, job.query)
-  } catch (e) {
-    error = e
-  }
-
-  await page.close()
-
-  if (error) {
-    throw {
+    return {
       time: new Date(),
-      value: error.message,
-      kind: 'error'
-    }
+      value: result,
+      kind: typeof result === 'string' ? 'text' : 'number'
+    };
   }
-
-  return {
-    time: new Date(),
-    value: result,
-    kind: typeof result === 'string' ? 'text' : 'number'
-  }
-}
+};
